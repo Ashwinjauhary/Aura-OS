e5a79a30a5c95f90f0385b54637ed845d70863cb
Ashwin Jauhary <ashwin2431333@gmail.com>
2026-03-02 22:39:25 +0530

    main

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ashwin.portfolio',
   appName: 'Aura OS',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'https://aura-os-theta.vercel.app/',
+    cleartext: true
+  }
 };
 
 export default config;
